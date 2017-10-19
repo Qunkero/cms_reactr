db944ee300eea665108cfaf7e84425515bda40b4
Qunkero <dron.andrew2011@yandex.ua>
2017-10-19 21:57:47 +0300

    set up environments

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    context: __dirname + '/frontend',
+    entry:   {
+        index: ['./index']
+    },
+    devtool: 'inline-source-map',
+    output:  {
+        path:       __dirname + '/public/script',
+        publicPath: '/script',
+        filename:   '[name].js'
+    },
+
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015','react', 'stage-0']
+            }
+        }]
+    },
+    devServer: {
+        // hot:true,
+        contentBase:__dirname + '/public',
+        host: 'localhost', // default
+        port: 3000 // default
+    }
+};
